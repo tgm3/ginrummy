3315dce766567ba6e51d84e26e200791ada9a98e
tgm3 <32113754+tgm3@users.noreply.github.com>
2018-11-08 18:28:28 -0600

    Create GameEngine.java

---
+++ src/GameEngine.java
@@ -0,0 +1,11 @@
+package com.example;
+
+
+public class GameEngine {
+
+    public static void main(String[] args) {
+        OneVsTwo.oneVsTwo(300);
+        TwoVsThree.twoVsThree(300);
+        OneVsThree.oneVsThree(300);
+    }
+}
